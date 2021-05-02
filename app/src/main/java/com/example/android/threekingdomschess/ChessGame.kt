219ef68cf8e4d0dd041b5a23b8a2a095465db6b2
Kevin La <chenchiaho@gmail.com>
2021-05-03 01:53:27 +0800

    finalize legal move logic for cannon

--- app/src/main/java/com/example/android/threekingdomschess/ChessGame.kt
+++ app/src/main/java/com/example/android/threekingdomschess/ChessGame.kt
@@ -1,6 +1,8 @@
 package com.example.android.threekingdomschess
 
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 object ChessGame {
 
@@ -87,38 +89,31 @@ object ChessGame {
         return false
     }
 
-    private fun canonVerticalPath(from: Square, to: Square): Boolean {
-        if (from.col != to.col) return false
-        val gap = abs(from.row - to.row) -1
-        if (gap == 0) return true
-        for (i in 1..gap) {
-            val nextRow = if (to.row > from.row) from.row + i +1 else from.row -i -1
-            if (piecePosition(Square(from.col, nextRow)) !=null) {
-                return false
+    private fun piecesBetween(fromCol: Int, fromRow: Int,
+                              toCol: Int, toRow: Int): Int {
+        var count = 0
+        val start: Int
+        val end: Int
+        if (fromCol == toCol) {
+            start = min(fromRow, toRow)
+            end = max(fromRow, toRow)
+            for (row in start + 1 until end) {
+                count += if (piecePosition(fromCol, row) == null) 0 else 1
+            }
+        } else {
+            start = min(fromCol, toCol)
+            end = max(fromCol, toCol)
+            for (col in start + 1 until end) {
+                count += if (piecePosition(col, fromRow) == null) 0 else 1
             }
         }
-        return true
+        return count
     }
 
-    private fun canonHorizontalPath(from: Square, to: Square): Boolean {
-        if (from.row != to.row) return false
-        val gap = abs(from.col - to.col) -1
-        if (gap == 0) return true
-        for (i in 1..gap) {
-            val nextCol = if (to.col > from.col) from.col + i +1 else from.col -i -1
-            if (piecePosition(Square(nextCol, from.row)) !=null) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private fun canonLegal(from: Square, to: Square): Boolean {
-        if (from.col == to.col && canonVerticalPath(from, to) ||
-                from.row == to.row && canonHorizontalPath(from, to)) {
-            return true
-        }
-        return false
+    private fun cannonLegal(from: Square, to: Square): Boolean {
+        return if (piecePosition(to.col, to.row) == null) {
+            rookLegal(from, to)
+        } else piecesBetween(from.col, from.row, to.col, to.row) == 1
     }
 
     private fun legalMove(from: Square, to: Square): Boolean {
@@ -126,16 +121,16 @@ object ChessGame {
             return false
         }
         val legalMove = piecePosition(from) ?: return false
-        when(legalMove.cType) {
-            ChessType.HORSE -> return horseLegal(from, to)
-            ChessType.ROOK -> return rookLegal(from, to)
-            ChessType.KING1 -> return rookLegal(from, to)
-            ChessType.KING2 -> return rookLegal(from, to)
-            ChessType.ADVISER -> return adviserLegal(from, to)
-            ChessType.PAWN1 -> return pawnLegal(from, to)
-            ChessType.PAWN2 -> return pawnLegal(from, to)
-            ChessType.GUARD -> return guardLegal(from, to)
-            ChessType.CANNON -> return canonLegal(from, to)
+        return when(legalMove.cType) {
+            ChessType.HORSE -> horseLegal(from, to)
+            ChessType.ROOK -> rookLegal(from, to)
+            ChessType.KING1 -> rookLegal(from, to)
+            ChessType.KING2 -> rookLegal(from, to)
+            ChessType.ADVISER -> adviserLegal(from, to)
+            ChessType.PAWN1 -> pawnLegal(from, to)
+            ChessType.PAWN2 -> pawnLegal(from, to)
+            ChessType.GUARD -> guardLegal(from, to)
+            ChessType.CANNON -> cannonLegal(from, to)
         }
         return true
     }
